b9635e5afb318cde7d4b17bf98c4b56dfaa857ff
BichTuyenNguyen <bichtuyen6897@gmail.com>
2018-12-01 13:44:13 +0700

    update asp.MVC login/register

---
+++ mvc/mvc/Models/UserAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="First name is required.")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
